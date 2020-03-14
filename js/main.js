22ed51bf454e05f2b3a1fe168ba85e77d205334f
gitradik <rminzp17@gmail.com>
2020-03-14 11:59:39 +0200

    first-scroll

--- js/main.js
+++ js/main.js
@@ -6,5 +6,24 @@ $(document).on('click', function (e){
       menu_opened === true){
           $('#navbarSupportedContent').collapse('toggle');
   }
+});
 
+$(document).ready(function(){
+  $(".input-mask-s").inputmask("+38(999) 999-99-99");
+});
+
+$(document).scroll(function() {
+  var docSrollTop = $(document).scrollTop();
+  var windowWidth = $(window).width();
+  if (windowWidth > 992) {
+    if (docSrollTop > 25) {
+      $('#headerMenu').css('display', 'none');
+    } else {
+      $('#headerMenu').css('display', 'flex');
+    }   
+  } else {
+    if ($('#headerMenu').css('display') !== 'flex') {
+      $('#headerMenu').css('display', 'flex');
+    }
+  }
 });
